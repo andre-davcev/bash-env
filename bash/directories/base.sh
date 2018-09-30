afa66d63d0cd80bc38a92a4ff15fbdaf5fcdf36a
Andre Davcev <>
2018-09-30 13:53:08 -0400

    Changing project alias

--- bash/directories/base.sh
+++ bash/directories/base.sh
@@ -4,7 +4,7 @@
 export desktop=${HOME}/Desktop
 export documents=${HOME}/Documents
 export downloads=${HOME}/Downloads
-export projects=${HOME}/Files
+export projects=${HOME}/Projects
 
 # ALIASES - DIRECTORIES 
 aliases desktop\
